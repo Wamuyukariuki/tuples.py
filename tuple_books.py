7b3906436851b2a719dc1f9a130582b8d2832afa
Wamuyukariuki <157091588+Wamuyukariuki@users.noreply.github.com>
2024-02-07 23:04:22 +0300

    Add files via upload

---
+++ tuple_books.py
@@ -0,0 +1,5 @@
+# creating a tuple containing my favourite books
+books_tuple = ('The River Between', 'Betrayal in the city', 'Rich dad Poor dad', 'Kifo Kisimani', 'Utengano')
+
+for items in books_tuple:
+    print(items)
